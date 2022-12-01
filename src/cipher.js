81fee874b6c4dad5854ec5781ecfcbcacf6307cc
Monica Romero <monicaromerogay@gmail.com>
2022-12-01 10:20:17 -0600

    github

--- src/cipher.js
+++ src/cipher.js
@@ -3,17 +3,9 @@ const cipher = {
 export default cipher; 
  //No borrar lo de arriba
 
-
-var alfabeto; 
-alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-// Necesito guardar los valores del mensaje
-
-
-//Necesito funcionalidad del botón "Cifrar Mensaje" ¿Cómo lo hago?
-//Necesito que "radio" se desmarque 
-
-var radio = docuement.querySelector("input[name=opcion]"), //¿por qué "radio" está en rojo? ¿Qué estoy haciendo mal? 
-
-radio.addEventListener('change', function) //¿Por qué el "." está en rojo? ¿este es el checked?
+ describe ('cipher',() => {
+    it ('should be an object', () => {
+        
+    }) 
+ })
 
