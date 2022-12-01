ea2c4c731cd521e03c40f2f843c7e3080b136ea5
Monica Romero <monicaromerogay@gmail.com>
2022-12-01 10:14:39 -0600

    cambios cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,21 @@
-const cipher = {
-  // ...
-};
 
-export default cipher;
+
+const cipher = {
+}
+export default cipher; 
+ //No borrar lo de arriba
+
+// Necesito guardar los valores del mensaje
+
+function cipher(str) {
+    var nuevoMensaje="";
+    for(var i=0;i<str.length;i++){
+        if(string.charCodeAt(i)>=65&&str.charCodeAt(i)<=90){
+            nuevoMensaje+=String.fromCharCode((str.charCodeAt(i)+13-65)%26+65);
+        }else{
+            nuevoMensaje+=String.fromCharCode(str.charCodeAt(i));
+        }
+    }
+    return nuevoMensaje;
+}
+cipher ("Hola")
