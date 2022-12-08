5acdbe0b91cc8a8b54ef491f1d15a1092a62b3ff
Monica Romero <monicaromerogay@gmail.com>
2022-12-07 18:10:40 -0600

    cambios 7 de Diciembre

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,6 @@
+//const cipher = {
+//}
 
-
-const cipher = {
-
-export default cipher; 
-}
-
-function encode (mensaje,desplazamiento) { // esta función establece los párametros de "codificar"
-    let resultado = ""; //el resultado es el mensaje que esperamos tener al final de la función y es una variable
-
-    for(let i=0;i<mensaje.length;i++){ //este es el loop, necesita 3 elementos = i=0="A", largo del mensaje que va a recorrer y el i++ que significa que añade +1
-
-      if(mensaje.charCodeAt(i)>=65 && mensaje.charCodeAt(i) <=90){ // ASCII A empieza en 65 y Z termina en 90
-         resultado+=String.fromCharCode((mensaje.charCodeAt(i)+ (desplazamiento-65))%26+65); // CharCode y CharCodeAt
-      }else{
-         resultado+=String.fromCharCode(mensaje.charCodeAt(i));
-      }
-    }
-
-    return resultado; 
-
-}
- //No borrar lo de arriba
+//export default cipher
 
 
