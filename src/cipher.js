0ae5e00cea8a8a84d4ae5108bdcf6c484f795bff
Monica Romero <monicaromerogay@gmail.com>
2022-12-13 10:26:02 -0600

    cambio del type of

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,13 @@
 const cipher = {
 
   encode(desplazamiento, mensaje) {// esta función establece los párametros de "codificar"
+    const desplazamiento1 = parseInt(desplazamiento)
     let resultado = ""; //el resultado es el mensaje que esperamos tener al final de la función y es una variable
     
-    if (typeof(desplazamiento) !== "number" || desplazamiento === 0 || typeof(mensaje) !== "string" || mensaje === "") {
-      throw TypeError("Error");
+    if (typeof desplazamiento1 !== "number" || typeof mensaje !== "string") {
+      throw new TypeError("El valor no es correcto", "cipher.js", 4);
     }
-    if (typeof desplazamiento === "object" || typeof mensaje === "object") {
-      throw TypeError("Error");
-    }
+
     for (let i = 0; i < mensaje.length; i++) { //este es el loop, necesita 3 elementos = i=0="A", largo del mensaje que va a recorrer y el i++ que significa que añade +1
       if (mensaje.charCodeAt(i) >= 65 && mensaje.charCodeAt(i) <= 90) {// ASCII A empieza en 65 y Z termina en 90
         resultado += String.fromCharCode((mensaje.charCodeAt(i) + (desplazamiento - 65)) % 26 + 65); // CharCode y CharCodeAt
@@ -22,12 +21,10 @@ const cipher = {
     
   decode(desplazamiento, mensaje) { // esta función establece los párametros de "codificar"
     let resultado = ""; //el resultado es el mensaje que esperamos tener al final de la función y es una variable
+    const desplazamiento2 = parseInt(desplazamiento)
 
-    if (typeof(desplazamiento) !== "number" || desplazamiento === 0 || typeof(mensaje) !== "string" || mensaje === "") {
-      throw TypeError("Error");
-    }
-    if (desplazamiento === null && mensaje === []) {
-      throw TypeError("Error");
+    if (typeof desplazamiento2 !== "number" || typeof mensaje !== "string") {
+      throw TypeError();
     }
 
     for (let i = 0; i < mensaje.length; i++) { //este es el loop, necesita 3 elementos = i=0="A", largo del mensaje que va a recorrer y el i++ que significa que añade +1
